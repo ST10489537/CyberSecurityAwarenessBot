748b3eb8526ed87d18dbb64f6652113872125e95
ST10489537 <ST10489537@vcconnect.edu.za>
2026-05-11 22:13:25 +0200

    Added WAV voice greeting playback functionality

--- CyberSecurityAwarenessBotGUIApp/MainWindow.xaml.cs
+++ CyberSecurityAwarenessBotGUIApp/MainWindow.xaml.cs
@@ -1,24 +1,17 @@
-﻿using System.Text;
+﻿using System.Media;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CyberSecurityAwarenessBotGUIApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Plays the chatbot voice greeting when the GUI opens.
+            SoundPlayer player = new SoundPlayer("Assets/Greeting.wav.wav");
+            player.Play();
         }
     }
 }
