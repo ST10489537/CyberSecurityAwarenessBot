9110fb5e9be077a3ff46d0141407c84e9d4938b5
ST10489537 <ST10489537@vcconnect.edu.za>
2026-06-22 12:30:01 +0200

    Created Part 3 task assistant interface

---
+++ CyberSecurityAwarenessBotGUIApp/Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberSecurityAwarenessBotGUIApp.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? ReminderDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
